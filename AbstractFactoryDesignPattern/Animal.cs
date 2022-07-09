8139d9f7c465e37fc5e32de34b27e2bdb854de13
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2022-07-09 15:39:36 +0600

    Creating Concrete Product

--- AbstractFactoryDesignPattern/Animal.cs
+++ AbstractFactoryDesignPattern/Animal.cs
@@ -10,4 +10,45 @@ namespace AbstractFactoryDesignPattern
     {
         string Speak();
     }
+
+
+
+
+    public class Cat : Animal
+    {
+        public string Speak()
+        {
+            return "Meow meow meow";
+        }
+    }
+    public class Lion : Animal
+    {
+        public string Speak()
+        {
+            return "Roar";
+        }
+    }
+    public class Dog : Animal
+    {
+        public string Speak()
+        {
+            return "Bark bark";
+        }
+    }
+
+    public class Octopus : Animal
+    {
+        public string Speak()
+        {
+            return "SQAWCK";
+        }
+    }
+
+    public class Shark : Animal
+    {
+        public string Speak()
+        {
+            return "Can not speak";
+        }
+    }
 }
